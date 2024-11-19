e5d3784bbd948bbb46ba78349579a1edcf30a99d
Adam Boesky <aboesky@college.harvard.edu>
2024-11-18 19:20:50 -0500

    Adding the basic simulation infrastructure, and a script that helps to write trajectories.

---
+++ sim/main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+
+
+class Agent {
+public:
+    Agent(double mass_kg, double thrustCapacity_N): mass(mass_kg), thrusterCapacity(thrustCapacity_N) {};
+
+
+    // Execute one step
+    void step(double dt) {
+
+        // Get the force vector
+        thrusterForce[0] = thrusterFiring * thrusterCapacity * sin(thrusterTheta);      // Fx = I(firing) * |F|sin(theta)
+        thrusterForce[1] = thrusterFiring * thrusterCapacity * cos(thrusterTheta);      // Fx = I(firing) * |F|cos(theta)
+
+        // Update the acceleration and velocity
+        for (int i=0; i < acceleration.size(); i++) {
+            acceleration[i] = thrusterForce[i] / mass;                                  // a = F/m
+            position[i] += (velocity[i] * dt) + (0.5 * acceleration[i] * dt * dt);      // x = x0 + v0*dt + (1/2)*a*dt^2
+            velocity[i] += acceleration[i] * dt;                                        // v = v0 + a*dt
+        }
+    }
+
+
+    // Log current state
+    void logState(const double currentTime, std::ofstream& logFile) {
+        logFile << currentTime << ",";
+        for (double r : position) {
+            logFile << r << ",";
+        }
+        for (double v : velocity) {
+            logFile << v << ",";
+        }
+        for (int i = 0; i < acceleration.size(); ++i) {
+            logFile << acceleration[i];
+            if (i < acceleration.size() - 1) {
+                logFile << ",";
+            }
+        }
+        logFile << "\n";
+    }
+
+private:
+    double mass;                                        // [kg]     Mass of the agent
+    double thrusterCapacity;                            // [N]      Magnitude of the force exerted by the thruster
+    bool thrusterFiring;                                // [bool]   Whether the thruster is firing
+    double thrusterTheta = 0.0;                         // [rad]    Angle between thruster and y-axis
+    std::vector<double> thrusterForce = {0.0, 0.0};     // [N]      Force vector exerted by the thruster
+    std::vector<double> position = {0.0, 0.0};          // [m]      Position of the agent (x, y)
+    std::vector<double> velocity = {0.0, 0.0};          // [m/s]    Velocity of the agent (vx, vy)
+    std::vector<double> acceleration = {0.0, 0.0};      // [m^2/s]  Acceleration of the agent (ax, ay)
+};
+
+
+class Controller {
+public:
+    void policy(Agent& agent) {
+        // IMPLEMENT POLICY
+    }
+};
+
+
+class Sim{
+public:
+    Sim(Agent agent, const Controller controller, const std::string& trajectoryFile, const std::string& logFileName): agent(agent), controller(controller) {
+
+        // Read in trajectory file
+        std::ifstream file(trajectoryFile);
+        if (file.is_open()) {
+            std::string line;
+            while (std::getline(file, line)) {
+                std::vector<double> row;
+                std::stringstream ss(line);
+                double value;
+                while (ss >> value) {
+                    row.push_back(value);
+                    if (ss.peek() == ',') {
+                        ss.ignore();
+                    }
+                }
+                trajectory.push_back(row);
+            }
+            file.close();
+        } else {
+            std::cerr << "Unable to open file: " << trajectoryFile << std::endl;
+        }
+
+        // Get the last time
+        t_final = trajectory[trajectory.size() - 1][0];
+
+        // Make a log file
+        logFile.open(logFileName);
+    }
+
+    // Run simulation!
+    void run_sim(const double dt) {
+
+        // Write log file header and initial log line
+        logFile << "time,x,y,velocity_x,velocity_y,acceleration_x,acceleration_y\n";
+        double time = 0;
+        agent.logState(time, logFile);
+
+        // Simulation loop
+        while (time < t_final) {
+            step(dt);
+            time += dt;
+            agent.logState(time, logFile);
+        }
+    };
+
+
+private:
+    Agent agent;
+    Controller controller;
+    std::vector<std::vector<double>> trajectory;
+    std::ofstream logFile;
+    double t_final;
+
+    void step(double dt) {
+
+        // Update the agent according to the policy and then take a step
+        controller.policy(agent);
+        agent.step(dt);
+    }
+};
+
+
+int main() {
+    // Create an agent with initial mass and thrust capacity
+    Agent agent(1000.0, 5000.0);
+
+    // Create a controller
+    Controller controller;
+
+    // Define the trajectory file and log file names
+    std::string trajectoryFile = "/Users/adamboesky/Research/RL_CMAES/trajectories/straight.csv";
+    std::string logFileName = "/Users/adamboesky/Research/RL_CMAES/sim_results/test.csv";
+
+    // Create a simulation
+    Sim simulation(agent, controller, trajectoryFile, logFileName);
+
+    // Run the simulation with a time step of 0.1 seconds
+    simulation.run_sim(0.1);
+
+    return 0;
+}
---
+++ trajectories/write_trajectories.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+import pandas as pd
+
+from typing import Tuple
+
+
+class Trajectory():
+    def __init__(self, trajectory_directory: str, t_final: Tuple[float], dt: float = 0.1):
+        """Parent class for trajectories.
+        
+        Args:
+            time_range: The final time value, in seconds.
+            dt: The timestep to create the trajectory for.
+        """
+        self.time_vector = np.arange(0, t_final + dt, step=dt)
+        self.trajectory_directory = trajectory_directory
+
+    def func(self) -> Tuple[np.ndarray, np.ndarray]:
+        """Function of self.time_vector that dictates how the trajectory looks. This function returns a tuple of numpy
+        arrays of the form (x_values, y_values).
+        
+        NOTE: Must be implemented in subclasses.
+        """
+        raise NotImplementedError('func() must be implemented in subclass!')
+
+    def write(self, fname: str):
+        x, y = self.func()
+        pd.DataFrame(
+            data={
+                'time': self.time_vector,
+                'x': x,
+                'y': y,
+            }
+        ).to_csv(os.path.join(self.trajectory_directory, fname), index=False)
+
+
+class StraightTrajectory(Trajectory):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def func(self):
+        return self.time_vector, self.time_vector
+
+
+def write_trajectories():
+    trajectory_dirpath = '/Users/adamboesky/Research/RL_CMAES/trajectories'
+
+    # Write a straight line trajectory
+    straight_traj = StraightTrajectory(trajectory_dirpath, t_final = 100)
+    straight_traj.write('straight.csv')
+
+
+if __name__=='__main__':
+    write_trajectories()
