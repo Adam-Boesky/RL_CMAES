62592c24f588ff8d3f4dfa22d15cda1e78d57cf9
jdinovi <john@dinovi.net>
2024-11-28 11:10:08 -0500

    more progress on cmaes

--- cmaes/cmaes.py
+++ cmaes/cmaes.py
@@ -5,6 +5,10 @@ import subprocess
 import os
 
 
+def norm(v):
+    ''' Computes the Euclidian norm of a vector. '''
+    return np.sqrt(sum([x*x for x in v]))
+
 CURRENT_TRAJECTORY = "straight"
 
 # Objective function
@@ -23,22 +27,21 @@ def objective_function(x):
     subprocess.run([command_to_run_sim, gamma, s, d], capture_output=True, text=True)
 
     # 2. Read in the actual and target trajectory values generated during the previous run
-    actual_trajectory = pd.read_csv("./sim_results/test.csv")
+    actual_trajectory = pd.read_csv("./sim_results/test.csv").iloc[:-1, :]
     target_trajectory = pd.read_csv(f"./trajectories/{CURRENT_TRAJECTORY}.csv")
 
     # 3. Calculate and return the loss values
-    loc_actual = actual_trajectory[['x']]
-    loc_target = target_trajectory[['x']]
-
-    n = len(loc_target)
-    loss = sum([abs(loc_actual.iloc[i] - loc_target.iloc[i]) for i in range(n)])
-
+    actual_positions = [np.array([row['x'], row['y']]) for _, row in actual_trajectory.iterrows()]
+    target_positions = [np.array([row['x'], row['y']]) for _, row in target_trajectory.iterrows()]
+    
+    n = len(target_positions)
+    loss = sum([norm(actual_positions[i] - target_positions[i]) for i in range(n)])
     return loss
 
 
 if __name__ == "__main__":
-    initial_mean = [0.5, 0.5, 0.5]
-    sigma = 0.5
+    initial_mean = [1.0, 1.0, 1.0]
+    sigma = 10
     
     es = cma.CMAEvolutionStrategy(initial_mean, sigma)
     
